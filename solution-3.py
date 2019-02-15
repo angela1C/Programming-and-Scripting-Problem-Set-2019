b9150f94d1393c3c7f2ce62ac44ab2f9601083b3
angela1C <fange21@icloud.com>
2019-02-15 15:26:13 +0000

    updated solution-3.py

--- solution-3.py
+++ solution-3.py
@@ -14,19 +14,18 @@ if yes then check if that number is also divisible by 12
 if the number is divisible by 6 but is not divisible by 12 return this number
 if the number is divisible by 6 and is also divisible by 12 do not return this
 
-check if it includes 10000. range see
+
+I initially put in extra print statements just to see if my code was working
+The program is only to print all the numbers that are divisible by 6  but not 12
 
 """
 
-for i in range(1000,1300):
+for i in range(1000,10000):
     if (i % 6 ==0):  # first checking if i is divisible by 6 using the modulus operator
-        print(i, " is divisible by 6")  # just for checking now
-        if(i % 12 ==0): # if this is true then check if the number is divisible by 12
-            print(i, " is divisible by 12")  ## just for checking now
-        else:
-            print(i, "is divisible by 6 but not by 12")
+        if(i % 12 !=0): # if the above is true then check if the number is divisible by 12
+            print(i)  ## just for checking now
+    
 
-# the program is only to print all the numbers that are divisible by 6  but not 12
 
 
     
