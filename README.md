2280dc5dc0c284493c0c02ced84d5bdcfba80b2e
angela1C <fange21@icloud.com>
2019-03-16 12:39:14 +0000

    updated README.md and also solution-3.py

--- README.md
+++ README.md
@@ -16,6 +16,41 @@ This repository contains my solutions to the Problem Set 2019 for the module Pro
 ## What each file contains
 
 ## References
+I have used the online documentation at the Python.org website for this project.
+In particular, The Python Tutorial has been my main reference point for learning the basics about the Python language. [The Python Tutorial](https://docs.python.org/3/tutorial/)
+It is available at the python.org websites at https://docs.python.org/3/tutorial/. 
+I have also referred to The Python Standard Library quite a bit following the links from The Python Tutorial.
+In addition I have looked to the PythonBook by Peter Spronk at http://spronck.net/pythonbook/pythonbook.pdf.
 
-https://docs.python.org/3/tutorial/
+## Solution-1.py
+This program contains my code for the first problem on the problem set.
+1. Write a program that asks the user to input any positive integer and 
+outputs the sum of all numbers between one and that number.
 
+To run this program, go to the command line and enter the following command:
+$ python solution-1.py
+The program will ask the user to input a postive integer.
+The program will output a positive integer containing the sum of all numbers betweem 1 and the number entered  by the user.
+
+## Solution-2.py
+
+This program contains my code for the second problem on the problem set.
+2. "Write a program that outputs whether or not today is a day that begins with the letter T. 
+
+To run this program, enter the following command on the command line:
+$ python solution-2.py
+
+The program does not require any input from the user.
+The program will output a message saying if today begins with a T or not.
+"Yes - Today begins with a T" or "No - today does not begin with a T"
+
+## Solution-3 .py
+
+This program contains my code for the third problem on the problem set.
+3. Write a program that prints all numbers between 1,000 and 10,000 that are divisible by 6 but not 12.
+
+To run this program, enter the following command on the command line:
+$ python solution-3.py
+
+The program does not require any input from the user.
+The program will print all the numbers betweem 1,000 and 10,000 that are divisible by 6 but are not divisible by 12. Any numbers between 1,000 and 10,000 that are divisible by both 6 and 12 are not printed
--- solution-3.py
+++ solution-3.py
@@ -1,5 +1,10 @@
 """
-Write a program that prints all numbers between 1,000 and 10,000 that are divisible by 6 but not 12.
+This program prints all numbers between 1,000 and 10,000 that are divisible by 6 but are not divisible by 12.
+
+This program contains my code for the third problem on the Problem Set 2019:
+3. Write a program that prints all numbers between 1,000 and 10,000 that are divisible by 6 but not 12.
+
+An example of running this program is as follows:
 
 $ python divisors.py
 1002
@@ -8,22 +13,39 @@ $ python divisors.py
 etc
 9990
 
-Takes all numbers betweem 1000 and 10000
-for each number checks if that number is divisible by 6
-if yes then check if that number is also divisible by 12
-if the number is divisible by 6 but is not divisible by 12 return this number
-if the number is divisible by 6 and is also divisible by 12 do not return this
+This program does not need any input from the user.
+It should take all of the numbers betweem 1000 and 10000 and for each number 
+check if that number is divisible by 6 and if that number is divisible by 6, then check if the number is also divisible by 12.
+If the number is divisible by 6 and is also divisible by 12 then return this number
+However if the number is divisible by 6 and is also divisible by 12 then do not return this number.
 
+Referring to [Section 4.2 for Statement](https://docs.python.org/3/tutorial/controlflow.html?highlight=range#for-statements) and [section 4.3. The Range() Function](https://docs.python.org/3/tutorial/controlflow.html?highlight=range#for-statements)
+of the Python Tutorial.
 
-I initially put in extra print statements just to see if my code was working
-The program is only to print all the numbers that are divisible by 6  but not 12
+A For statement is used to iterate through every number in a sequence of numbers in the order they appear in the sequence.
+The `range()` function generates a sequence of numbers that can be iterated over in the for loop.
 
+I am assuming from the sample output provided that between 1,000 and 10,000 means starting at 1,000 and up to but not including 10,000
 """
 
+# I am using a `for` statement to iterate through every number from 0 to 1000 in order.
+# Using range() to generate a sequence of all integer numbers beginning at 1000, up to but not including 10,000
+# `i` is the name of a variable set up to hold each value of the numbers generated from range()
+# and for each value of i in the range, execute the statements indented after the :
+# https://docs.python.org/3/tutorial/controlflow.html?highlight=range#the-range-function
+
 for i in range(1000,10000):
-    if (i % 6 ==0):  # first checking if i is divisible by 6 using the modulus operator
-        if(i % 12 !=0): # if the above is true then check if the number is divisible by 12
-            print(i)  ## just for checking now
+    # using an if statement to check if the value of `i` at each interation is divisible by 6 or not
+    # Using the modulus operator `%` to check if the number is divisible by 6. `==` is the equality operator
+    if (i % 6 ==0):  
+        ## then check if i is not divisible by 12, again using the modulus operator. `!=` means not equal to
+        ## This if clause below is only executed if the if statement above it evaluates to true
+        if(i % 12 !=0): 
+            # if the number is divisible by 6 and is not divisible by 12 then print i
+            print(i)  
+
+"""
+This code is producing the same output as in the sample output expected.
     
 
 
